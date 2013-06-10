8d86e7c174611fd19454945b55c116802e4b40fb
manos <savemanos@gmail.com>
2013-06-10 17:56:14 +0200

    Added tracking readding previously droppped links from django admin

--- poxweb/ext/dbpoll.py
+++ poxweb/ext/dbpoll.py
@@ -41,7 +41,7 @@ class Dbpoll(Task) :
             log.debug(len(oldslivers))
             log.debug(len(newslivers))
             if len(newlinks) < len(oldlinks) and len(oldslivers) == len(newslivers):
-                log.debug("New Links")
+                log.debug("Rem Links")
                 #newlinks = self.objList2dict(newlinks)
                 log.debug(newlinks)
                 # Find the deleted links
@@ -50,7 +50,11 @@ class Dbpoll(Task) :
                 #log.debug(dellinks)
                 # Delete the removed links
                 self.del_links(dellinks)
-
+            elif len(newlinks) > len(oldlinks) and len(oldslivers) == len(newslivers):
+                log.debug("Add Links")
+                addlinks = self.diff_list(newlinks,oldlinks)
+                log.debug(addlinks)
+                self.add_links(addlinks)
             # Outside the if to catch also new links
             # not only dropped ones
             oldlinks = newlinks
@@ -83,6 +87,21 @@ class Dbpoll(Task) :
             self.of_send_link_down(dpid1,mac2)
             self.of_send_link_down(dpid2,mac1)
 
+    def add_links(self,links):
+        # Get info from the deleted links
+        for link in links:
+            log.debug('Inside add Links')
+            # This is the db representation id for sliver 1
+            sliver1 = str(link['sliver1_id'])
+            # Get all the info from the model
+            sliver1,node1,dpid1,mac1 = db.get_sliver_info(sliver1)
+            # This is the db representation id for sliver 1
+            sliver2 = str(link['sliver2_id'])
+            # Get all the info from the model
+            sliver2,node2,dpid2,mac2 = db.get_sliver_info(sliver2)
+            # Send the delete link OF messages
+            self.of_send_link_up(dpid1,mac2)
+            self.of_send_link_up(dpid2,mac1)
     
     # Get mac from sliver model as dict
     def get_mac_from_sliver(self,sliver):
@@ -112,6 +131,29 @@ class Dbpoll(Task) :
                 #fm.actions.append(of.ofp_action_output( port=of.OFPP_NONE ))
                 connection.send(fm)
 
+    def of_send_link_up(self,dpid,rmac):
+        for connection in core.openflow.connections:
+            if connection.dpid == dpid :
+                # Parse rmac the OF way
+                rmac = EthAddr(rmac)
+                log.debug('Got you!')
+                # Block in Node 1 incoming from node 2
+                fm = of.ofp_flow_mod()
+                fm.match.dl_src = rmac
+                fm.command = of.OFPFC_DELETE
+                # Many OpenVswitch  versions do not support OFPP_NONE
+                # Instead of the no action is defined
+                # hence an empty action
+                #fm.actions.append(of.ofp_action_output( port=of.OFPP_NONE ))
+                connection.send(fm)
+                # Block in Node 1 outcoming to node 2
+                fm = of.ofp_flow_mod()
+                fm.match.dl_dst = rmac
+                fm.command = of.OFPFC_DELETE
+                #fm.actions.append(of.ofp_action_output( port=of.OFPP_NONE ))
+                connection.send(fm)
+    
+    
     # Convert Django dbobject to Dict List 
     def objList2dict(self,objList):
         dictList = []
